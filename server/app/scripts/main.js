673bb65ce9314cd53404b9516a3f90ae6c950c7c
Maya Driver <madriver@deloitte.co.uk>
2015-11-06 11:03:33 +0000

    feature/COB-101 - messing around with getting locations

--- server/app/scripts/main.js
+++ server/app/scripts/main.js
@@ -18,6 +18,8 @@ $(document).ready(function(){
 		});
 		$('#playerName').val('');
 	});
+	getPlayerLocation('Maya');
+
 });
 
 db.on('child_added', function(snapshot) {
@@ -34,4 +36,28 @@ function playerCount() {
 		var count = snapshot.numChildren();
 		$('.js-player-count').text('count: ' + count);
 	});
+}
+
+function getPlayer(name) {
+	var player = "s";
+	db.orderByChild('player').equalTo(name).on('child_added', function(snapshot) {
+		var player = "hello";
+	});
+	console.log(player);
+	return player;
+}
+
+function getPlayerLocation(name) {
+	var location = getPlayer("Maya");
+	console.log(getPlayer("Maya"));
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+          c(lat1 * p) * c(lat2 * p) * 
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
